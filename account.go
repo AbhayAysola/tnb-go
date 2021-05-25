d332c90bb5e97047cdc2b9ceea04774c0aa0c3c3
Abhay <67417765+AbhayAysola@users.noreply.github.com>
2021-05-25 09:20:07 +0530

    Add create random account logic

--- account.go
+++ account.go
@@ -15,13 +15,22 @@ type Account struct {
 }
 
 func CreateAccount(signingKeyHex string) (Account, error) {
-	signingKeyByte, err := hex.DecodeString(signingKeyHex)
-	if err != nil {
-		return Account{}, err
+	if signingKeyHex != "" {
+
+		signingKeyByte, err := hex.DecodeString(signingKeyHex)
+		if err != nil {
+			return Account{}, err
+		}
+		accountNumber, signingKey, err := sign.Keypair(bytes.NewReader(signingKeyByte))
+		if err != nil {
+			return Account{}, err
+		}
+		return Account{signingKey: signingKey, accountNumber: accountNumber, SigningKeyHex: signingKeyHex, AccountNumberHex: hex.EncodeToString(accountNumber)}, nil
+	} else {
+		accountNumber, signingKey, err := sign.Keypair(nil)
+		if err != nil {
+			return Account{}, nil
+		}
+		return Account{signingKey: signingKey, accountNumber: accountNumber, SigningKeyHex: (hex.EncodeToString(signingKey))[0:64], AccountNumberHex: hex.EncodeToString(accountNumber)}, nil
 	}
-	accountNumber, signingKey, err := sign.Keypair(bytes.NewReader(signingKeyByte))
-	if err != nil {
-		return Account{}, err
-	}
-	return Account{signingKey: signingKey, accountNumber: accountNumber, SigningKeyHex: signingKeyHex, AccountNumberHex: hex.EncodeToString(accountNumber)}, nil
 }
