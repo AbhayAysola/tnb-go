f5287c8bd0370e029452320d028b1d067ed4c182
Abhay <67417765+AbhayAysola@users.noreply.github.com>
2021-05-25 10:09:43 +0530

    add signature creation and verification, verify keypair

--- account.go
+++ account.go
@@ -2,15 +2,16 @@ package account
 
 import (
 	"bytes"
+	"crypto"
 	"encoding/hex"
 
 	"github.com/kevinburke/nacl/sign"
 )
 
 type Account struct {
-	signingKey sign.PrivateKey
-	accountNumber sign.PublicKey
-	SigningKeyHex string
+	SigningKey       sign.PrivateKey
+	accountNumber    sign.PublicKey
+	SigningKeyHex    string
 	AccountNumberHex string
 }
 
@@ -25,12 +26,49 @@ func CreateAccount(signingKeyHex string) (Account, error) {
 		if err != nil {
 			return Account{}, err
 		}
-		return Account{signingKey: signingKey, accountNumber: accountNumber, SigningKeyHex: signingKeyHex, AccountNumberHex: hex.EncodeToString(accountNumber)}, nil
+		return Account{SigningKey: signingKey, accountNumber: accountNumber, SigningKeyHex: signingKeyHex, AccountNumberHex: hex.EncodeToString(accountNumber)}, nil
 	} else {
+
 		accountNumber, signingKey, err := sign.Keypair(nil)
 		if err != nil {
 			return Account{}, nil
 		}
-		return Account{signingKey: signingKey, accountNumber: accountNumber, SigningKeyHex: (hex.EncodeToString(signingKey))[0:64], AccountNumberHex: hex.EncodeToString(accountNumber)}, nil
+		return Account{SigningKey: signingKey, accountNumber: accountNumber, SigningKeyHex: (hex.EncodeToString(signingKey))[0:64], AccountNumberHex: hex.EncodeToString(accountNumber)}, nil
 	}
+}
+
+func (account Account) CreateSignature(message string) (string, error) {
+	signatureByte, err := account.SigningKey.Sign(nil, []byte(message), crypto.Hash(0))
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(signatureByte), err
+}
+
+func VerifySignature(signatureHex string, accountNumberHex string) bool {
+	accountNumber, err := hex.DecodeString(accountNumberHex)
+	if err != nil {
+		return false
+	}
+	signature, err := hex.DecodeString(signatureHex)
+	if err != nil {
+		return false
+	}
+	return sign.PublicKey(accountNumber).Verify(signature)
+}
+
+func VerifyKeyPair(signingKeyHex string, accountNumberHex string) bool {
+	signingKey, err := hex.DecodeString(signingKeyHex)
+	if err != nil {
+		return false
+	}
+	accountNumber, err := hex.DecodeString(accountNumberHex)
+	if err != nil {
+		return false
+	}
+	accountNumberTest, _, err := sign.Keypair(bytes.NewReader(signingKey))
+	if err != nil {
+		return false
+	}
+	return bytes.Equal(accountNumber, accountNumberTest)
 }
